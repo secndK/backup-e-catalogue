0a10e99416e7a74111ee156ba275b8bbe9d271c2
secndk <jilmay02@gmail.com>
2025-08-03 21:19:55 +0000

    mise a jour du template

---
+++ resources/views/components/voidgrubs.blade.php
@@ -0,0 +1,8 @@
+ <div class="pagetitle">
+     <h1>@yield('pageTitle')</h1>
+     <nav>
+         <ol class="breadcrumb">
+             <li class="breadcrumb-item"><a href="index.html">@yield('currentPage')</a></li>
+         </ol>
+     </nav>
+ </div><!-- End Page Title -->
